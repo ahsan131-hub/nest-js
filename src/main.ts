0a4c132965091c7d58c2fa75f492644be1aa39b4
Muhammad Ahsan <ahsanayan131@gmail.com>
2024-01-20 15:37:21 +0500

    initialize nestjs project

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Nestjs Learning')
+      .setDescription('The nestjs learning API description')
+      .setVersion('1.0')
+      .addTag('learning')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+bootstrap();
